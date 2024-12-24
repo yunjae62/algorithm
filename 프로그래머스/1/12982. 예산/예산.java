8af4d4125cf9a456fd6a0327c33e7ad5e75f33f6
김재윤 <55584664+yunjae62@users.noreply.github.com>
2024-12-24 14:40:57 +0900

    [level 1] Title: 예산, Time: 2.39 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,16 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int totalCost = 0;
-        Queue<Integer> q = new PriorityQueue<>();
+        Arrays.sort(d);
         
-        for (int num : d) {
-            q.add(num);
-        }
-        
-        while (!q.isEmpty() && totalCost + q.peek() <= budget) {
-            int cost = q.poll();
-            totalCost += cost;
+        for (int cost : d) {
+            budget -= cost;
+            
+            if (budget < 0) {
+                break;
+            }
+            
             answer++;
         }
         
