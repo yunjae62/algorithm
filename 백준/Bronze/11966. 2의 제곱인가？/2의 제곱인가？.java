8ed8f21d9a1b8e9b29546d7feb29c5b514318d41
김재윤 <55584664+yunjae62@users.noreply.github.com>
2024-12-31 01:17:27 +0900

    [Bronze III] Title: 2의 제곱인가?, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/11966. 2의 제곱인가？/2의 제곱인가？.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+
+        while (N % 2 == 0) {
+            N /= 2;
+        }
+
+        System.out.println(N == 1 ? 1 : 0);
+    }
+}
