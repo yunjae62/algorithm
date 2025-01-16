96a6de74e3498b2776ce46d42a365535a040417b
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-01-16 16:33:20 +0900

    [level 2] Title: 택배상자, Time: 164.78 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        int idx = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 1; i <= order.length; i++) {
+            stack.push(i);
+            
+            while (!stack.isEmpty() && stack.peek() == order[idx]) {
+                answer++;
+                idx++;
+                stack.pop();
+            }
+        }
+        
+        return answer;
+    }
+}
