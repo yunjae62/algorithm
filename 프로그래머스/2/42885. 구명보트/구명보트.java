846fe9633fdc27d10dc61a7ecd36026f3de078c6
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-01-23 15:18:58 +0900

    [level 2] Title: 구명보트, Time: 17.13 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,13 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int min = 0;
+        int start = 0;
         
         Arrays.sort(people);
         
-        for (int max = people.length - 1; min <= max; max--) {
-            if (people[min] + people[max] <= limit) 
-                min++;
+        for (int end = people.length - 1; start <= end; end--) {
+            if (people[start] + people[end] <= limit) {
+                start++;
+            }
+            
             answer++;
         }
         
