cdc4a3c4cae0d97aa57aefeb2a152683be046ae0
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-02-03 18:38:51 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,11 +1,9 @@
-SELECT 
-    animal_id, 
-    name, 
+SELECT
+    ANIMAL_ID,
+    NAME,
     CASE
         WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
         ELSE 'X'
-    END 중성화
-from 
-    animal_ins
-order by 
-    animal_id;
+    END "중성화"
+FROM
+    ANIMAL_INS;
