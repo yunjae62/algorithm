9287a84bca518ba3fd97b7ded91f242b6de4177a
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-02-19 17:59:39 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 716 ms, Memory: 119040 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,18 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int stoi(String s) {
-        return Integer.parseInt(s);
-    }
 
     static int N, M;
     static int[][] dp;
     static StringBuilder sb = new StringBuilder();
 
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
         M = stoi(st.nextToken());
@@ -20,21 +23,23 @@ public class Main {
         dp = new int[N + 1][N + 1];
 
         for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
+
             for (int j = 1; j <= N; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + stoi(st.nextToken());
+                dp[i][j] = stoi(st.nextToken()) + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
             }
         }
 
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
+
             int x1 = stoi(st.nextToken());
             int y1 = stoi(st.nextToken());
             int x2 = stoi(st.nextToken());
             int y2 = stoi(st.nextToken());
 
-            int res = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
-            sb.append(res).append('\n');
+            int sum = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+            sb.append(sum).append("\n");
         }
 
         System.out.println(sb);
