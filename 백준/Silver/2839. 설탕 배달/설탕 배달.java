6000e0ceedb2fda12eee1a7cb6a2d5a2a1c67073
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-02-19 16:45:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,19 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    static int N;
+    static int[] dp;
 
-        if (N == 4 || N == 7) {
-            System.out.println(-1);
-        } else if (N % 5 == 0) {
-            System.out.println(N / 5);
-        } else if (N % 5 == 1 || N % 5 == 3) {
-            System.out.println(N / 5 + 1);
-        } else if (N % 5 == 2 || N % 5 == 4) {
-            System.out.println(N / 5 + 2);
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+
+        dp = new int[Math.max(N, 5) + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[3] = dp[5] = 1;
+
+        for (int i = 6; i <= N; i++) {
+            if (dp[i - 3] == Integer.MAX_VALUE && dp[i - 5] == Integer.MAX_VALUE) {
+                continue;
+            }
+
+            dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
         }
+
+        System.out.println(dp[N] == Integer.MAX_VALUE ? -1 : dp[N]);
     }
 }
