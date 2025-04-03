4abfd52955e662fd5cb7ad9cda9aa06a688d5390
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-04-03 14:46:12 +0900

    [Silver I] Title: 안전 영역, Time: 280 ms, Memory: 55984 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,67 +1,97 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N;
+    static int[][] graph;
+    static int answer;
+    static int height = 0, maxHeight = 1;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
 
-    static int N, max = Integer.MIN_VALUE, min = 0, res = 0;
-    static int[][] map;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
-        map = new int[N][N];
+
+        graph = new int[N][N];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                int x = stoi(st.nextToken());
-                map[i][j] = x;
-                max = Math.max(max, x);
+                graph[i][j] = stoi(st.nextToken());
+                maxHeight = Math.max(maxHeight, graph[i][j]);
             }
         }
 
-        while (min <= max) {
-            boolean[][] visited = new boolean[N][N];
-            int cnt = 0;
+        while (height <= maxHeight) {
+            visited = new boolean[N][N];
+            int count = 0;
+
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (!visited[i][j] && map[i][j] > min) {
-                        cnt++;
-                        bfs(i, j, min, visited);
+                    if (!visited[i][j] && graph[i][j] > height) {
+                        bfs(i, j);
+                        count++;
                     }
                 }
             }
-            res = Math.max(res, cnt);
-            min++;
+
+            answer = Math.max(answer, count);
+            height++;
         }
 
-        System.out.println(res);
+        System.out.println(answer);
     }
 
-    static void bfs(int n, int m, int height, boolean[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{n, m});
-        visited[n][m] = true;
+    static void bfs(int ox, int oy) {
+        Queue<Node> q = new LinkedList<>();
+        visited[ox][oy] = true;
+        q.add(new Node(ox, oy));
 
         while (!q.isEmpty()) {
-            int[] pos = q.poll();
-            int x = pos[0], y = pos[1];
+            Node now = q.poll();
+            int x = now.x;
+            int y = now.y;
+
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (0 <= nx && nx < N && 0 <= ny && ny < N && !visited[nx][ny] && map[nx][ny] > height) {
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny});
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
                 }
+                if (visited[nx][ny]) {
+                    continue;
+                }
+                if (graph[nx][ny] <= height) {
+                    continue;
+                }
+
+                visited[nx][ny] = true;
+                q.add(new Node(nx, ny));
             }
+        }
+    }
+
+    static class Node {
+
+        int x, y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
