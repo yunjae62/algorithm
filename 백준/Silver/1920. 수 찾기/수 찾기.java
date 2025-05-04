f5983b1083f0b65189e239d57a8a2e971b49eb6d
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-05-04 14:37:03 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 50396 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,24 +1,39 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N, M;
+    static Set<Integer> set = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        for (int ele : input) {
-            map.put(ele, 1);
+        N = stoi(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(stoi(st.nextToken()));
         }
-        int M = Integer.parseInt(br.readLine());
-        int[] output = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        for (int ele : output) {
-            bw.write(map.getOrDefault(ele, 0) + "\n");
+
+        M = stoi(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int X = stoi(st.nextToken());
+            sb.append(set.contains(X) ? "1" : "0").append("\n");
         }
-        bw.close();
+
+        System.out.println(sb);
     }
 }
