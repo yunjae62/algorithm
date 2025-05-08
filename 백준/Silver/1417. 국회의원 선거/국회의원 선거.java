e2f80fa07affb6384b7fd3e235a3b5fe406832c0
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-05-08 16:44:26 +0900

    [Silver V] Title: 국회의원 선거, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,35 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N, one, count;
+    static Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
     static int stoi(String s) {
         return Integer.parseInt(s);
     }
-
-    static int N, answer, vote;
-    static Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = stoi(st.nextToken());
+        one = stoi(br.readLine());
 
-        vote = stoi(br.readLine());
         for (int i = 0; i < N - 1; i++) {
             pq.add(stoi(br.readLine()));
         }
 
-        while (!pq.isEmpty() && vote <= pq.peek()) {
-            int top = pq.poll();
-
-            top--;
-            answer++;
-            vote++;
-
-            pq.add(top);
+        while (!pq.isEmpty() && one <= pq.peek()) {
+            one++;
+            count++;
+            pq.add(pq.poll() - 1);
         }
 
-        System.out.println(answer);
+        System.out.println(count);
     }
 }
