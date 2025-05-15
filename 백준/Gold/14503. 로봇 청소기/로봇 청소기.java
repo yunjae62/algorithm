7ab14961613c6f3e10327335ed9280af709b9d49
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-05-15 18:04:53 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,89 +1,98 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int stoi(String s) {
-		return Integer.parseInt(s);
-	}
 
-	static int N, M, res = 0;
-	static int[][] graph;
-	static boolean[][] visited;
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
+    static int N, M, answer;
+    static Robot robot;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] graph;
+    static boolean[][] cleaned;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
 
-		N = stoi(st.nextToken());
-		M = stoi(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine(), " ");
-		int r = stoi(st.nextToken());
-		int c = stoi(st.nextToken());
-		int d = stoi(st.nextToken());
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
 
-		graph = new int[N][M];
-		visited = new boolean[N][M];
+        st = new StringTokenizer(br.readLine());
+        int r = stoi(st.nextToken());
+        int c = stoi(st.nextToken());
+        int d = stoi(st.nextToken());
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < M; j++) {
-				graph[i][j] = stoi(st.nextToken());
-			}
-		}
-		dfs(r, c, d);
-	}
+        graph = new int[N][M];
+        cleaned = new boolean[N][M];
+        robot = new Robot(r, c, d);
 
-	static void dfs(int ox, int oy, int d) {
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = stoi(st.nextToken());
+            }
+        }
 
-		if (!visited[ox][oy]) {
-			res++;
-		}
-		visited[ox][oy] = true;
+        while (true) {
+            if (!cleaned[robot.x][robot.y]) {
+                cleaned[robot.x][robot.y] = true;
+                answer++;
+                continue;
+            }
 
-		boolean isRotate = false;
-		for (int i = 0; i < 4; i++) {
-			int nx = ox + dx[i];
-			int ny = oy + dy[i];
+            boolean isClean = true;
 
-			if ((0 <= nx && nx < N) && (0 <= ny && ny < M)) {
-				if (graph[nx][ny] == 0 && !visited[nx][ny]) {
-					isRotate = true;
-				}
-			}
-		}
+            for (int i = 0; i < 4; i++) {
+                int nx = robot.x + dx[i];
+                int ny = robot.y + dy[i];
 
-		if (isRotate) {
-			for (int i = 0; i < 4; i++) {
-				d = (d - 1 + 4) % 4;
+                if ((0 <= nx && nx < N) && (0 <= ny && ny < M) && (graph[nx][ny] == 0) && (!cleaned[nx][ny])) {
+                    isClean = false;
+                }
+            }
 
-				int nx = ox + dx[d];
-				int ny = oy + dy[d];
+            if (!isClean) {
+                robot.d = (robot.d - 1 + 4) % 4;
 
-				if ((0 <= nx && nx < N) && (0 <= ny && ny < M)) {
-					if (graph[nx][ny] == 0 && !visited[nx][ny]) {
-						visited[nx][ny] = true;
-						res++;
-						dfs(nx, ny, d);
-					}
-				}
-			}
-		} else {
-			int bx = ox + dx[(d + 2) % 4];
-			int by = oy + dy[(d + 2) % 4];
+                int nx = robot.x + dx[robot.d];
+                int ny = robot.y + dy[robot.d];
 
-			if (graph[bx][by] == 1) {
-				System.out.println(res);
-				System.exit(0);
-			}
+                if ((0 <= nx && nx < N) && (0 <= ny && ny < M) && (graph[nx][ny] == 0) && (!cleaned[nx][ny])) {
+                    robot = new Robot(nx, ny, robot.d);
+                }
+                continue;
+            }
 
-			if (!visited[bx][by]) {
-				res++;
-			}
-			visited[bx][by] = true;
-			dfs(bx, by, d);
-		}
-	}
+            int backD = (robot.d + 2) % 4;
+
+            int nx = robot.x + dx[backD];
+            int ny = robot.y + dy[backD];
+
+            if ((0 <= nx && nx < N) && (0 <= ny && ny < M) && (graph[nx][ny] == 0)) {
+                robot = new Robot(nx, ny, robot.d);
+                continue;
+            }
+
+            break;
+        }
+
+        System.out.println(answer);
+    }
+
+    static class Robot {
+
+        int x, y, d;
+
+        public Robot(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
+    }
 }
