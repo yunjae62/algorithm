7c7d25f0aed0104e8fb1fd155e8a53d1f96e1a6c
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-06-08 14:06:20 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1340 ms, Memory: 81928 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    static int stoi(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = stoi(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int x : arr) {
+            sb.append(x).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
