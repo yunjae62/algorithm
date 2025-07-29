abb00a135820f1cd444cbd10ae2c3de2379dce8c
김재윤 <55584664+yunjae62@users.noreply.github.com>
2025-07-29 22:20:15 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.08 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        if (my_string.length() < is_suffix.length()) {
-            return 0;
-        }
-        
-        String word = my_string.substring(my_string.length() - is_suffix.length());
-        
-        if (word.equals(is_suffix)) {
-            return 1;
-        }
-        
-        return 0;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
